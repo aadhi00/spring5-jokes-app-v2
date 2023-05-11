68c64ad04f8d7d5e262cd1b90be8b804be8a6581
aadhithyan <aadhithyanaadhi@gmail.com>
2023-05-11 19:23:05 +0530

    created joke controller. closes #3

--- src/main/java/com/aadhi/chucknorris/controllers/JokeController.java
+++ src/main/java/com/aadhi/chucknorris/controllers/JokeController.java
@@ -1,4 +1,18 @@
 package com.aadhi.chucknorris.controllers;
 
+import com.aadhi.chucknorris.services.JokeService;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 public class JokeController {
+    private final JokeService jokeService;
+
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+    @RequestMapping({"/",""})
+    public String showJoke(Model model){
+        model.addAttribute("joke", jokeService.fetchJoke());
+        return "index";
+    }
 }
