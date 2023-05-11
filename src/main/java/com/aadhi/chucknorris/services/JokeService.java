1447e5b2fc769f770aa2a477674a7ba8c3838726
aadhithyan <aadhithyanaadhi@gmail.com>
2023-05-11 13:55:17 +0530

    created joke service. closes #2

--- src/main/java/com/aadhi/chucknorris/services/JokeService.java
+++ src/main/java/com/aadhi/chucknorris/services/JokeService.java
@@ -1,13 +1,5 @@
 package com.aadhi.chucknorris.services;
 
-import guru.springframework.norris.chuck.ChuckNorrisQuotes;
-
-public class JokeService {
-    ChuckNorrisQuotes chuckNorrisQuotes;
-    public JokeService(){
-        chuckNorrisQuotes = new ChuckNorrisQuotes();
-    }
-    public String fetchJoke(){
-        return chuckNorrisQuotes.getRandomQuote();
-    }
+public interface JokeService {
+    String fetchJoke();
 }
